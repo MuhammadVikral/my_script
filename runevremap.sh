1142a8f13f6c4b0e9da31b50f12620d3753a98f6
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-02-18 15:26:52 +0700

    fixin

--- runevremap.sh
+++ runevremap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s evremap -c ~/
+	tmux new-session -ds evremap -c ~/
 	exit 0
 fi
 if ! tmux has-session -t=evremap 2>/dev/null; then
 	tmux new-session -ds evremap -c ~/
+	tmux send-keys -t evremap "sudo evremap remap evremap_config.toml" Enter
+	tmux send-keys -t evremap "021095" Enter
+else
+	tmux kill-session -t evremap
 fi
-
-tmux send-keys -t evremap "sudo evremap remap evremap_config.toml" Enter
-tmux send-keys -t evremap "021095" Enter
--- runmouseleses.sh
+++ runmouseleses.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s mouseless -c ~/
-	exit 0
-fi
 if ! tmux has-session -t=mouseless 2>/dev/null; then
 	tmux new-session -ds mouseless -c ~/
+	tmux send-keys -t mouseless "sudo mouseless --config ~/.config/mouseless/config.yaml" Enter
+	tmux send-keys -t mouseless "021095" Enter
+else
+	tmux kill-session -t mouseless
 fi
-
-tmux send-keys -t mouseless "sudo mouseless --config ~/.config/mouseless/config.yaml" Enter
-tmux send-keys -t mouseless "021095" Enter
