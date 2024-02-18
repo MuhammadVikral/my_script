0013de6f3991ff1bb8601eddac34d746142daa4a
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-02-18 16:55:19 +0700

    modify f-watch

--- f-watch.sh
+++ f-watch.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+#grep list of app in flutter app
+if [[ $# -eq 1 ]]; then
+	selected=$1
+else
+	selected=$(find app/ -mindepth 1 -maxdepth 1 -type d | fzf)
+fi
+if [[ -z $selected ]]; then
+	exit 0
+fi
+
 dirName=$(basename "$PWD" | tr . _)
-selected_name="nodemon_${dirName}"
-tmux send-keys "fvm flutter run $4 $2 $3 $4 --pid-file=/tmp/tf1.pid" Enter
+selected_app=$(basename "$selected" | tr . _)
+selected_name="flutter_runner_${dirName}_${selected_app}"
+tmux_running=$(pgrep tmux)
+
+#create new session dedicated for running the flutter app
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+	tmux new-session -s $selected_name -c $selected
+	exit 0
+fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	tmux new-session -ds $selected_name
-	tmux send-keys -t $selected_name 'npx -y nodemon --watch . -e dart -x "cat /tmp/tf1.pid | xargs kill -s USR1"' Enter
+	tmux new-session -ds $selected_name -c $selected
 fi
+
+#create new window dedicated for running the nodemon
+if ! tmux list-windows -t $selected_name -F '#{window_name}' | grep -q '^nodemon$'; then
+	# Create a new window named "nodemon"
+	tmux new-window -t $selected_name -n "nodemon"
+fi
+
+#run the flutter run and nodemon watch and then switch to runner client
+tmux send-keys -t $selected_name:1 "fvm flutter run $4 $2 $3 $4 --pid-file=/tmp/tf1.pid" Enter
+tmux send-keys -t $selected_name:nodemon 'npx -y nodemon --watch . -e dart -x "cat /tmp/tf1.pid | xargs kill -s USR1"' Enter
+tmux switch-client -t "$selected_name:1"
